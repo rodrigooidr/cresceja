2886af5f66d406dfbae8977bad87197dc00e6283
rodrigooidr <rodrigooidr.oliveira@gmail.com>
2025-09-08 19:54:02 -0300

    Update jest.config.js

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -2,20 +2,27 @@
 module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/test/setupTests.ts'],
+
+  // Transpila TS/JS/JSX/TSX e também .mjs/.cjs via babel-jest
   transform: {
-    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
+    '^.+\\.(mjs|cjs|ts|tsx|js|jsx)$': 'babel-jest',
   },
-  // transpilar dependências ESM usadas nos testes (MSW e cia)
+
+  // Não ignore estes módulos ESM em node_modules (deixe o Babel transpilar)
+  // Se outro pacote acusar erro "Unexpected token 'export'", inclua-o aqui.
   transformIgnorePatterns: [
-    '/node_modules/(?!(msw|@mswjs|nanoid|uuid|@reduxjs/toolkit|@bundled-es-modules)/)',
+    '/node_modules/(?!(msw|@mswjs|@bundled-es-modules|headers-polyfill|is-node-process|strict-event-emitter|cookie|urlpattern-polyfill|whatwg-url|fetch-blob|formdata-polyfill|web-streams-polyfill|data-urls|undici|nanoid|uuid|@reduxjs/toolkit)/)',
   ],
+
   moduleNameMapper: {
-    // CSS modules
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    // alias
     '^@/(.*)$': '<rootDir>/src/$1',
   },
-  moduleDirectories: ['node_modules', '<rootDir>/src'],
+
+  // Ajuda o resolver do Jest a lidar com ESM
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'mjs', 'cjs', 'json', 'node'],
+  extensionsToTreatAsEsm: ['.mjs'],
+
   testPathIgnorePatterns: ['/node_modules/', '/dist/', '/build/'],
   collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/test/**'],
 };
